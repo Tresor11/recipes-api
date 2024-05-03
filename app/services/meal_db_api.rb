7a726eda9e2c4778f27b1610b1db76324bb08a03
Tresor11 <bireketresor@gmail.com>
2024-05-03 20:08:07 +0300

    Add recipe controller

--- app/services/meal_db_api.rb
+++ app/services/meal_db_api.rb
@@ -4,4 +4,16 @@ class MealDbApi
   def fetch_categories
     HTTParty.get('https://www.themealdb.com/api/json/v1/1/categories.php')
   end
+
+  def fetch_meals_by_category(category)
+    HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=#{category}")
+  end
+
+  def fetch_meal_by_id(id)
+    HTTParty.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{id}")
+  end
+
+  def fetch_recipe_by_id(id)
+    HTTParty.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{id}")
+  end
 end
